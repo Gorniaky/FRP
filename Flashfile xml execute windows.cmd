79a54258d466d0557d5ba4e231e207528e8c5369
Gorniaky <30408913+Gorniaky@users.noreply.github.com>
2021-07-21 10:58:37 -0300

    Add files via upload

--- Flashfile xml execute windows.cmd
+++ Flashfile xml execute windows.cmd
@@ -1,31 +1,29 @@
 @echo off
-if exist flashfile.xml (goto startconvert)
+if exist flashfile.xml (goto:startconvert)
 find "flashfile" flashfile.xml
 pause
 exit
 :startconvert
-findstr "\<software_version\>" flashfile.xml > software_version.txt
+findstr "\<software_version\>" flashfile.xml>software_version.txt
 for /f delims^=^"^ tokens^=^2 %%a in (software_version.txt) do set title=%%a
 del software_version.txt
 title %title%
 echo @title %title%>flashfile.cmd
-findstr "\<flash\>" flashfile.xml > flash.txt
-for /F delims^=^"^ tokens^=4^,6^,8 %%a in (flash.txt) do echo @if not exist %%a echo %%a not exist^&set notexistfiles=1 >> flashfile.cmd
+findstr "\<flash\>" flashfile.xml>flash.txt
+for /F delims^=^"^ tokens^=4 %%a in (flash.txt) do echo @if not exist %%a echo %%a not exist^&set notexistfiles=1>>flashfile.cmd
 (echo @if defined notexistfiles pause^>nul^&exit
 echo mfastboot getvar max-sparse-size
 echo mfastboot oem fb_mode_set)>>flashfile.cmd
-for /F delims^=^"^ tokens^=4^,6^,8 %%a in (flash.txt) do echo mfastboot %%b %%c %%a >> flashfile.cmd
+for /F delims^=^"^ tokens^=4^,6^,8 %%a in (flash.txt) do echo mfastboot %%b %%c %%a>>flashfile.cmd
 del flash.txt
-findstr "\<erase\>" flashfile.xml | findstr /v "modem" > erase.txt
-for /F delims^=^"^ tokens^=2^,4^,6 %%a in (erase.txt) do echo mfastboot %%a %%b >> flashfile.cmd
+findstr "\<erase\>" flashfile.xml|findstr /v "modem">erase.txt
+for /F delims^=^"^ tokens^=2^,4^,6 %%a in (erase.txt) do echo mfastboot %%a %%b>>flashfile.cmd
 del erase.txt
 (echo mfastboot oem fb_mode_clear
 echo mfastboot reboot
-echo @cmd) >> flashfile.cmd
-findstr /v "@title" flashfile.cmd > "flashfile %title%.txt"
-findstr /v "@if" "flashfile %title%.txt" > "FLASHFILE COMMANDS %title%.txt"
-del "flashfile %title%.txt"
-if exist mfastboot.exe (goto startflash)
+echo @cmd)>>flashfile.cmd
+findstr /v "@title" flashfile.cmd|findstr /v "@if">"FLASHFILE COMMANDS %title%.txt"
+if exist mfastboot.exe (goto:startflash)
 exit
 :startflash
 pause
